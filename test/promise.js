b923df21fbfde3a627d0d1ee8eaf935e2b2b89d9
Rebecca Turner <turner@mikomi.org>
2014-03-13 12:08:09 -0400

    Refactor tests and take advantage of object identity for better diagnostics

--- test/promise.js
+++ test/promise.js
@@ -3,71 +3,76 @@
 var Promise = require('../index.js'),
     test  = require('tape');
 
+var createWith = function(F){ return function(msg,cb) { var P = Promise(F); cb ? cb(P,P+" "+msg) : msg(P) } }
 var create = {
     immediate: {
-        success: function () { return Promise(function(R) { R.fulfill('OK') }) },
-        failure: function () { return Promise(function(R) { throw new Error('NOK') }) },
+        success: createWith(function(R) { R.fulfill('OK') }),
+        failure: createWith(function(R) { throw new Error('NOK') })
     },
     deferred: {
-        success: function () { return Promise(function(R) { process.nextTick(function(){ R.fulfill('OK') }) }) },
-        failure: function () { return Promise(function(R) { process.nextTick(function(){ R.reject(new Error('NOK')) }) }) },
+        success: createWith(function(R) { process.nextTick(function(){ R.fulfill('OK') }) }),
+        failure: createWith(function(R) { process.nextTick(function(){ R.reject(new Error('NOK')) }) })
     },
 };
 
-function map(A,F) { for (var K in A) F(K,A[K]) }
+function forEach(A,F) { for (var K in A) F(K,A[K]) }
 
 test("Promise Style Tests", function(T) {
     T.plan(25);
-    map( create, function(kind,statuses) {
-        map( statuses, function(result,create) {
-            (function (){
-                var M="basic then/catch "+kind + " " + result;
+    forEach( create, function(kind,statuses) {
+        forEach( statuses, function(result,create) {
+            create("basic then/catch "+kind + " " + result,function (P,M){
                 result == 'success'
-                    ? create().then(function(V) { T.is(V,'OK',M) })
-                    : create().catch(function(E) { T.ok(E instanceof Error,M) });
-            })();
-            (function (){
-                var M="basic then(do,else) "+kind + " " + result;
+                    ? P.then(function(V) { T.is(V,'OK',M) })
+                    : P.catch(function(E) { T.ok(E instanceof Error,M) });
+            });
+            create("basic then(do,else) "+kind + " " + result, function (P,M){
                 result == 'success'
-                    ? create().then(function(V) { T.is(V,'OK',M) },function(E) { T.fail(M) })
-                    : create().then(function(V) { T.fail(M) },function(E) { T.ok(E instanceof Error,M) });
-            })();
-            (function (){
-                var M="basic chained thens and catches "+kind + " " + result;
+                    ? P.then(function(V) { T.is(V,'OK',M) },function(E) { T.fail(M) })
+                    : P.then(function(V) { T.fail(M) },function(E) { T.ok(E instanceof Error,M) });
+            });
+            create(function (P){
+                var chained = result == 'success' ? P.then(function(V){}) : P.catch(function(E){});
+                var M = chained + " "+"basic chained thens and catches "+kind + " " + result;
                 result == 'success'
-                    ? create().then(function(V){}).then(function(V) { T.is(V,'OK',M) })
-                    : create().catch(function(E){}).catch(function(E) { T.ok(E instanceof Error,M) });
-            })();
-            (function(){
-                var M="then chained to catch "+kind+" "+result;
+                    ? chained.then(function(V) { T.is(V,'OK',M) })
+                    : chained.catch(function(E) { T.ok(E instanceof Error,M) });
+/*
                 result == 'success'
-                    ? create().then(function (V){ T.is(V,'OK',M) }).catch(function(E){ T.fail(M) })
-                    : create().then(function (V){ T.fail(M) }).catch(function(E){ T.ok(E instanceof Error,M)});
-            })();
-            (function(){
-                var M="catch chained to then "+kind+" "+result;
+                    ? P.then(function(V){}).then(function(V) { T.is(V,'OK',M) })
+                    : P.catch(function(E){}).catch(function(E) { T.ok(E instanceof Error,M) });
+*/
+            });
+            create("then chained to catch "+kind+" "+result, function(P,M){
                 result == 'success'
-                    ? create().catch(function(E){ T.fail(M) }).then(function (V){ T.is(V,'OK',M) })
-                    : create().catch(function(E){ T.ok(E instanceof Error,M) }).then(function (V){ T.fail(M) });
-            })();
+                    ? P.then(function (V){ T.is(V,'OK',M) }).catch(function(E){ T.fail(M) })
+                    : P.then(function (V){ T.fail(M) }).catch(function(E){ T.ok(E instanceof Error,M)});
+            });
+            create("catch chained to then "+kind+" "+result, function(P,M){
+                result == 'success'
+                    ? P.catch(function(E){ T.fail(M) }).then(function (V){ T.is(V,'OK',M) })
+                    : P.catch(function(E){ T.ok(E instanceof Error,M) }).then(function (V){ T.fail(M) });
+            });
             if ( result == 'success' ) {
-                create().then(function (V){ return "STILL OK"})
-                        .then(function (V){ T.is(V,"STILL OK", "chained w/success->new val" ) });
+                create(function(P) {
+                    var CP = P.then(function (V){ return "STILL OK"});
+                    CP.then(function (V){ T.is(V,"STILL OK", CP+" chained w/success->new val" ) });
+                });
             }
         })
     })
-    Promise(function(R){ R.withoutErrors("OK") })
-            (function(E,V){ T.is(V,'OK', 'resolve without errors') });
+    var WoE = Promise(function(R){ R.withoutErrors("OK") });
+    WoE.then(function(V){ T.is(V,'OK', WoE+' resolve without errors') });
 
-    var C1 = Promise(function(R){ R(null,'OK') });
-    Promise(function(R){ R(C1) })
-            (function(E,V){ T.is(V,'OK', 'chained continuable resolve') });
+    var Chained = Promise.fulfill(Promise.fulfill('OK'))
+    Chained.then(function(V){ T.is(V,'OK', Chained+' chained promise resolve') });
 
     var msg = 'Promise that fails without catch throws the error';
     require('domain').create()
-        .on('error', function(E){ T.is(E,'boom',msg) })
+        .on('error', function(E){  T.is(E.message,'boom',msg) })
         .run(function() {
-            Promise(function(R){ R('boom') })
-                .then(function(V){ T.fail(msg) })
+            var P = Promise(function(R){ R(new Error('boom')) });
+            msg = P+" "+msg;
+            P.then(function(V){ T.fail(msg) })
         });
 });
